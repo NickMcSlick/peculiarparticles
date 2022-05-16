c05b6a592286a4dd1e94ce88f02a53f339562ee2
NickMcSlick <84112025+NickMcSlick@users.noreply.github.com>
2022-05-15 20:50:51 -0500

    Add files via upload

--- js/peculiarParticles.js
+++ js/peculiarParticles.js
@@ -32,18 +32,17 @@
 /*********************/
 
 const V_SHADER_SOURCE = `#version 300 es
-	in vec2 a_p;		// Position
+	in vec4 a_p;		// Position
 	in float a_p_s;		// Point size
 	
-	uniform mat4 u_m_mat;	// Model matrix
-	
 	void main() {
-		gl_Position = u_m_mat * a_p;
-		gl_Pointsize = a_p_s;
+		gl_Position = a_p;
+		gl_PointSize = a_p_s;
 	}
 `
 
 const F_SHADER_SOURCE = `#version 300 es
+	precision highp float;
 	out vec4 out_c; // Output color
 	
 	uniform vec4 u_c;	// Input color
@@ -56,14 +55,42 @@ const F_SHADER_SOURCE = `#version 300 es
 // Main program
 function main() {
 	let canvas = document.getElementById("canvas");
+	let webGL = canvas.getContext("webgl2");
 	
-	console.log(canvas);
+	initShaders(webGL, V_SHADER_SOURCE, F_SHADER_SOURCE);
+	
+	let particle = new Particle(1, [0, 0], [0, 0], [, 0, 0, 1]);
+	
+	drawParticle(webGL, particle);
 }
 
-function Particle() {
-	this.size = 1.0;
-	this.position = new Array(2);
-	this.velocity = new Array(4);
-	this.color = new Array(4);
+function drawParticle(gl, particle) {
+	// Vertex shader pointers
+	let a_p = gl.getAttribLocation(gl.program, "a_p");
+	let a_p_s = gl.getAttribLocation(gl.program, "a_p_s");
+	
+	// Fragment shader pointers
+	let u_c = gl.getUniformLocation(gl.program, "u_c");
+	
+	gl.vertexAttrib4f(a_p, particle.position[0], particle.position[1], 0.0, 0.0);
+	gl.vertexAttrib1f(a_p_s, particle.size);
+	gl.uniform4f(u_c, particle.color[0], particle.color[1], particle.color[2], particle.color[3]);
+
+	followCursor(particle);
+	
+	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+	gl.drawArrays(gl.POINTS, 0, 1);
+}
+
+function followCursor(particle) {
+	
+}
+
+function Particle(size, position, velocity, color) {
+	this.size = size;
+	this.position = position;
+	this.velocity = velocity;
+	this.color = color;
 }
 
